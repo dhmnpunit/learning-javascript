c661d3db27dff6ca5dd7884a46e99086570684d5
Punit <spunit2025@gmail.com>
2024-07-25 21:50:41 +0530

    learning javascript completed notes till - type conversion

---
+++ notes.js
@@ -0,0 +1,220 @@
+/*  BASICS
+
+console.log(`Hello`);
+console.log(`i like pizza`);
+
+window.alert(`this is an alert`);
+window.alert(`i like pizza`);
+
+this is a comment
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like pizza!`;
+
+*/
+
+
+
+
+
+
+
+
+
+
+/*  VARIABLES 
+
+variable = a container that stores a value.
+           behaves as if it were the value it contains.
+
+1. declaration      let x;
+2. assignment       x = 100;
+
+
+let x;
+x = 123;
+console.log(x);
+
+let age = 25;
+console.log(age);
+
+let price = 10.99;
+console.log(price);
+
+let gpa = 2.1;
+console.log(gpa);
+
+let firstName = "Punit";
+
+let favouriteFood = "pizza";
+
+let email = "bro@gmail.com"
+
+
+console.log(`you are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is: ${gpa}`);
+console.log(typeof age);
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`You like ${favouriteFood}`);
+console.log(`Your email is ${email}`);
+
+
+let online = true;
+let forSale = false;
+let student = true;
+
+console.log(`Bro is online: ${online}`);
+console.log(`Is this car for sale: ${forSale}`);
+console.log(`Enrolled: ${student}`);
+
+
+let fullName = "Punit Dhiman";
+let age = 17;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+/* ARITHEMATIC OPERATORS
+
+arithematic operatoes = operands (values, variables, etc.)
+                        operators (+ - * /)
+                        ex. 11 = x + 5;
+
+
+
+    let students = 30;
+
+// students = students + 2;
+// students = students - 2;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// let extraStudents = students % 3;
+
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+
+// students++;
+// students--;
+
+
+{   
+    OPERATOR PRECEDENCE
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & modulo 
+    4. addition % subtrsaction 
+}
+
+
+
+let result = 1 + 2 * 3 + 4 ** 2;
+
+
+console.log(result);
+
+
+*/
+
+
+
+
+
+
+
+
+
+/*  HOW TO ACCEPT USER INPUT
+    
+    1. EASY WAY = window prompt
+    2. PROFESSIOL WAY = HTML textbox
+
+
+1.  EASY WAY 
+
+    let username; 
+    username = window.prompt(`What's your username?`);
+    console.log(username);
+
+
+2.  PROFESSIONAL WAY
+
+    let username;
+    document.getElementById("mySubmit").onclick = function(){
+        username = document.getElementById("myText").value;
+        document.getElementById("WelcomeText").textContent = `Hello ${username}`
+    }
+
+*/
+
+
+
+
+
+
+
+
+
+/*  TYPE CONVERSION
+
+type conversion = change the datatype pf a value to another
+                  (stings, numbers, booleans)
+
+
+
+                let age = window.prompt("How old are you?");
+                age = Number(age);
+                age += 1;
+                console.log(age, typeof age);
+
+
+                let x = "0";
+                let y = "0";
+                let z = "0";
+
+                x = Number(x);
+                y = String(y);
+                z = Boolean(z);
+
+                console.log(x, typeof x);
+                console.log(y, typeof y);
+                console.log(z, typeof z);
+
+
+*/
+
+
+
+
+
+git 
+
+
+
+/*  CONSTANTS
+
+
+
+*/
