4fd68094b3dd50c4e7a5c92cfd9eea8536eedcac
Punit <spunit2025@gmail.com>
2024-07-27 23:44:57 +0530

    completed the string slicing

--- notes.js
+++ notes.js
@@ -577,3 +577,70 @@ console.log(letterGrade);
 
 */
 
+
+
+
+
+
+
+
+
+
+/*  STRING METHODS 
+    allow you to manipulate and work with text (strings)
+
+let userName = "PunitDhiman";
+let newName = "punit     ";
+
+// .charAt() 
+console.log(userName.charAt(5));
+
+// .indexOf() .lastIndexOf()
+console.log(userName.indexOf("i"));
+console.log(userName.lastIndexOf("i"));
+
+// .length()
+console.log(userName.length);
+
+// .trim()
+newName = newName.trim();
+console.log(newName);
+
+// .toUpperCase()
+console.log(userName.toUpperCase());
+
+// .toLowerCase()
+console.log(userName.toLowerCase());
+
+// .repeat()
+console.log(userName.repeat(3));
+
+// .startsWith()
+console.log(userName.startsWith("P"));
+
+// .endsWith()
+console.log(userName.endsWith("n"));
+
+// .includes()
+console.log(userName.includes("D"));
+
+
+
+let phoneNumber = "123-456-7890";
+
+// .replaceAll()
+phoneNumber = phoneNumber.replaceAll("-", "/");
+console.log(phoneNumber);
+
+// .padStart()
+phoneNumber = phoneNumber.padStart(15, "0");
+console.log(phoneNumber);
+
+// .padEnd()
+phoneNumber = phoneNumber.padEnd(20, "0");
+console.log(phoneNumber);
+
+*/
+
+
+
