3fb47ba2c5a62de44daee670b8bf7a1214f4018a
Punit <spunit2025@gmail.com>
2024-07-28 13:58:24 +0530

    built a number guessging game

--- notes.js
+++ notes.js
@@ -848,3 +848,27 @@ while(!loggedIn) {
 */
 
 
+
+
+
+
+
+
+
+
+/*  FOR LOOP
+    repeat some code a LIMITED amount of time
+
+for(let i = 0; i <=20; i++) {
+
+    if(i == 13) {
+        continue;  // to skip an iteration
+    } else {
+        console.log(i);
+
+    }
+
+}
+
+*/
+
