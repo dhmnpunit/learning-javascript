71ef574ff113bb04c42b46c1c946576aedb22eea
Punit <spunit2025@gmail.com>
2024-07-28 13:26:48 +0530

    completed for loop

--- notes.js
+++ notes.js
@@ -807,3 +807,44 @@ if(PI !== "3.14") {
 
 
 
+
+
+
+
+
+
+
+/*  WHILE LOOP 
+    repeat some code WHILE some condition is true
+
+let userName = "";
+
+while(userName === "" || null) {
+    userName = window.prompt(`Enter your name`);
+
+}
+
+console.log(`Hello ${userName}`);
+
+
+
+let loggedIn = false;
+let userName;
+let password;
+
+while(!loggedIn) {
+    userName = window.prompt(`Enter your username`)
+    password = window.prompt(`Enter your password`)
+
+    if(userName === "myUsername" && password === "myPassword") {
+        loggedIn = true;
+        console.log("you are logged in!");
+    } else {
+        console.log("invalid credentials! Please try again!");
+    }
+}
+
+
+*/
+
+
