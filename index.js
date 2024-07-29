aa15e8ef641607c701f6ab47f766159e004ebefd
Punit <spunit2025@gmail.com>
2024-07-29 17:28:13 +0530

    made a dice roller program

--- index.js
+++ index.js
@@ -1,74 +1,28 @@
-/*  REST PARAMETERS
-    (...rest) allow a function work with a variable 
-    number of arguments by bundling them into an array
-
-    spread = expands an array into separate elements
-    rest = bundles separate elements into an array
+/*  DICE ROLLER PROGRAM
 
 
 
-    */
-
-
-// const food1 = "pizza";
-// const food2 = "hamburger";
-// const food3 = "fries";
-// const food4 = "pasta";
-// const food5 = "sushi";
-
-// function openFridge(...foods) {
-//     console.log(...foods);
-// }
-
-// function getFood(...foods) {
-//     return foods;
-// }
-
-// // openFridge(food1, food2, food3, food4, food5);
-
-// const foods = getFood(food1, food2, food3, food4, food5);
-// console.log(foods);
+*/
 
 
 
+function rollDice() {
 
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
 
-function sum(...numbers) {
-
-    let result = 0;
-    for(let number of numbers) {
-        result += number;
+    for(let i = 0; i < numOfDice; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="dice-images/${value}.png" alt="Dice ${value}" >`)
     }
-    return result;
+
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
+
+    console.log(values);
+
 }
-
-const total = sum(1, 2, 3, 4, 5, 6, 7, 8);
-
-console.log(total);
-
-
-
-
-function getAverage(...numbers) {
-    
-    let result = 0;
-    for(let number of numbers) {
-        result += number;
-    }
-    return result / numbers.length;
-}
-
-const average = getAverage(1, 2, 3, 4, 5, 6, 7, 8);
-
-console.log(average);
-
-
-
-
-
-function combineStrings(...strings) {
-    return strings.join(" ");
-}
-
-const fullName = combineStrings("Mr.", "Spongebob", "Squarepants", "III" )
-console.log(fullName);
