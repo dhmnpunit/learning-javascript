08b6e6a1b7a09c1d1af1292647b926b003955e71
Punit <spunit2025@gmail.com>
2024-07-29 16:11:50 +0530

    completed rest parameters

--- notes.js
+++ notes.js
@@ -1166,3 +1166,44 @@ console.log(splitArray);
 
 */
 
+
+
+
+
+
+
+
+
+
+/*  FIXME: SPREAD OPERATOR 
+    ... allows an iterable such as an array or string 
+    to be expanded in separate elements
+    (unpack the elements)
+
+let numbers = [1, 2, 3, 4, 5]
+let max = Math.max(...numbers);
+let min = Math.min(...numbers);
+
+console.log(max);
+console.log(min);
+
+
+
+let userName = "Punit";
+let letters = [...userName].join("-");;
+
+console.log(letters);
+
+
+
+let fruits = ["apple", "orange", "banana"];
+let vegetables = ["carrot", "celery", "potatoes"];
+
+
+let foods = [...fruits, ...vegetables, "eggs", "milk"];
+
+console.log(foods);
+
+*/
+
+
