1f73463f66ad3a70d4a7270ecde09241ee63d224
Punit <spunit2025@gmail.com>
2024-07-29 15:56:54 +0530

    completed spread operator

--- notes.js
+++ notes.js
@@ -1032,7 +1032,7 @@ function function2() {
 
 
 
-/*  TEMPERATURE CONVERSION PROGRAM
+/*  FIXME: TEMPERATURE CONVERSION PROGRAM
 
 const textBox = document.getElementById('textBox');
 const toFahrenheit = document.getElementById('toFahrenheit');
@@ -1059,4 +1059,110 @@ function convert(){
 
 */
 
+
+
+
+
+
+
+
+
+
+/*  FIXME: ARRAY
+    a variable like structure that can hold 
+    more than 1 value
+
+
+let fruits = ["apple", "orange", "banana",];
+
+console.log(fruits);  // prints the whole array
+
+
+// TODO: prints the nth element of the array
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);  // this will print undefined because array is 0-indexed
+
+
+fruits[2] = "coconut";
+console.log(fruits); 
+
+
+// TODO: adding a new element to the end of the array
+fruits.push("mango");
+console.log(fruits);
+
+
+// TODO: adding a new element to the beginning of the array
+fruits.unshift("kiwi");
+console.log(fruits);
+
+
+// TODO: removing the first element from the array
+fruits.shift();
+console.log(fruits);
+
+
+// TODO: removing the last element from the array
+fruits.pop();
+console.log(fruits);
+
+
+// TODO: prints the number of elements in the array
+console.log(fruits.length);  
+
+
+// TODO: finding the index of an element
+let index = fruits.indexOf("grape");
+console.log(index);
+
+
+// TODO: looping through an array
+for(let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}  
+
+
+// TODO: sort the array
+fruits.sort();
+console.log(fruits);
+
+
+// TODO: sort the array in reverse
+fruits.sort().reverse();
+console.log(fruits);
+
+
+// TODO: reversing the array
+fruits.reverse();
+console.log(fruits);
+
+
+// TODO: adding an element at a specific index
+fruits.splice(1, 0, "grape");
+console.log(fruits);
+
+
+// TODO: removing an element at a specific index
+fruits.splice(2, 1);
+console.log(fruits);
+
+
+// TODO: checking if an array contains an element
+let containsGrape = fruits.includes("grape");
+console.log(containsGrape);
+
+
+// TODO: converting an array to a string
+let stringifiedArray = fruits.join(", ");
+console.log(stringifiedArray);
+
+
+// TODO: converting a string to an array
+let splitArray = stringifiedArray.split(", ");
+console.log(splitArray);
+
+
+*/
 
