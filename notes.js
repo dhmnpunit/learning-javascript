0b2ca3a036a3a67d5f3b45ebb80c0caf9a7768cf
Punit <spunit2025@gmail.com>
2024-07-31 20:01:54 +0530

    completed constructors

--- notes.js
+++ notes.js
@@ -1844,3 +1844,48 @@ person1.eat();
 
  */
 
+
+
+
+
+
+
+
+
+
+/*  THIS
+    reference to the object where THIS is used
+    (the object depends on the immediate context)
+    person.name = this.name
+
+// TODO:
+const person1 = {
+    name: "Punit",
+    favFood: "Pizza",
+    sayHello: function () {
+        console.log(`Hi! I am ${this.name} and i like ${this.favFood}.`);
+    },
+    eat: function () {
+        console.log(`${this.name} is eating ${this.favFood}.`);
+    }
+}
+
+// TODO:
+const person2 = {
+    name: "Prince",
+    favFood: "Burgers",
+    sayHello: function () {
+        console.log(`Hi! I am ${this.name} and i like ${this.favFood}.`);
+    },
+    eat: function () {
+        console.log(`${this.name} is eating ${this.favFood}.`);
+    }
+}
+
+person1.sayHello();
+person1.eat();
+person2.sayHello();
+person2.eat();
+
+ */
+
